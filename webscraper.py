2e97c8b42befa8529b954f430ff0e28033a7c6e3
Boyd K <herobrine75@live.com>
2020-08-27 14:59:17 +1000

    figured out how to sort throught the scrapped data

--- webscraper.py
+++ webscraper.py
@@ -5,10 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 url = 'https://www.wunderground.com/history/daily/YSSY/date/2020-8-26'
-driver = webdriver.Chrome(executable_path="C:\\Users\\herob\\Documents\\GitHub\\weatherboi\\chromedriver.exe")
+driver = webdriver.Chrome()#executable_path="C:\\Users\\herob\\Documents\\GitHub\\weatherboi\\chromedriver.exe")
 driver.get(url)
 tables = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "mat-table.cdk-table.mat-sort.ng-star-inserted")))
 for table in tables:
     newTable = pd.read_html(table.get_attribute('outerHTML'))
     if newTable:
-        print(newTable[0].fillna(''))
+        #gather temp data
+        print(newTable[0].fillna('')['Temperature'][1].rstrip(" F"))
+        #gather humidity data
+        #gather dew point data
+        #gather wind speed data
+        #gather Pressure data
+        #See if it rained in the afternoon
